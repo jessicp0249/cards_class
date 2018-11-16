94e811c6a27dac9edf590389001a018dd1486f43
Matthew Herrera <herreram81266@student.vvc.edu>
2018-11-15 19:43:47 -0800

    update

--- card.cpp
+++ card.cpp
@@ -8,17 +8,32 @@ using namespace std;
 
 Card::Card(int val)
 {
-    m_value = val;
+    m_id = val;
 }
 
 int Card::get_value()
 {
+    m_value = m_id % 13;
     return m_value;
 }
 
 string Card::get_suit()
 {
-    return m_suit;
+    int i = m_id / 13;
+    if(i = 1)
+    {
+        m_suit = "Clubs";
+    } else if (i = 2)
+    {
+        m_suit = "Spades";
+    } else if (i = 3)
+    {
+        m_suit = "Hearts";
+    } else 
+    {
+        m_suit = "Diamonds";
+    }
+        return m_suit;
 }
 
 void Card::print()
