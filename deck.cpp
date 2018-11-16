ee3a15fb71b65d6d414c20d8291d33426f952043
Colby Hillers <colbyhillers@cis.vvc.edu>
2018-11-15 19:22:34 -0800

    deck.h updated, deck.cpp stubbed

---
+++ deck.cpp
@@ -0,0 +1,21 @@
+#include "card.h"
+#include "deck.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void Deck:add_card(const Card &c)
+{
+    deck.push_back(c);
+}
+
+void Deck::shuffle() const
+{
+     
+}
+
+vector<Card> Deck::deal(int n)
+{
+    
+}
--- deck.h
+++ deck.h
@@ -1,1 +1,20 @@
+#ifndef "DECK_H"
+#define "DECK_H"
+#include <vector>
+#include <string>
+#include "card.h"
 
+using namespace std;
+
+class Deck
+{
+public:
+    void Deck::add_card(const Card &c);
+    void Deck::shuffle() const;
+    vector<Card> Deck::deal(int n);
+
+private:
+    vector<Card> deck;
+};
+
+#endif
