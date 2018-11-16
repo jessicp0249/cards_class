81858d91f29ce2fcdaeb72f95057ecdd8527687b
Matthew Herrera <herreram81266@student.vvc.edu>
2018-11-15 19:19:57 -0800

    update

--- card.h
+++ card.h
@@ -1,1 +1,24 @@
+#ifndef CARD_H
+#define CARD_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Card
+{
+    public:
+        Card();
+        Card(int val);
+
+        int get_value();
+        string get_suit;
+    
+        void print();
+
+    private:
+        int m_value;
+        string m_suit;
+
+};
